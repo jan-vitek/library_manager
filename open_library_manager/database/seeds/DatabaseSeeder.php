fbd5e0c6d025177863a93c85d6d6f8d5e427103d
Sanquira <sanquira.van.delbar@gmail.com>
2015-03-24 13:27:04 +0100

    Added database seed. 5 titles - 1 bachelor, 1 journal, 3 books 5 authors 3 titles_type
    1 author has 2 books
    2 authors have 1 book
    1 author has 1 journal
    1 author has 1 bachelor thesis

--- open_library_manager/database/seeds/DatabaseSeeder.php
+++ open_library_manager/database/seeds/DatabaseSeeder.php
@@ -14,7 +14,11 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('AuthorsTableSeeder');
+		$this->call('Author_titleTableSeeder');
+		$this->call('TitlesTableSeeder');
+		$this->call('Title_typesTableSeeder');
+		
 	}
 
 }
