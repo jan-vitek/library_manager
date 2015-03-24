1ef9e6718a1d1666f4d3cb22dffc08dfd2227236
Jan Vitek <vitek@library.lan>
2015-03-24 13:42:07 +0100

    filled in Author.php

--- open_library_manager/app/Author.php
+++ open_library_manager/app/Author.php
@@ -4,6 +4,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model {
 
-	//
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['name', 'birth_year', 'password'];
+	
+	
+        public function titles()
+        {
+            return $this->belongsToMany('App\Title', 'author_title');
+        }
 
 }
